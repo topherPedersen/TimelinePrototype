da8e37c1ba58d87b10057155fefe52787a102a8f
Chris Pedersen <chrispedatx@gmail.com>
2024-04-07 18:44:19 -0500

    rename variable

--- App.tsx
+++ App.tsx
@@ -5,25 +5,31 @@ import {
 
 interface TimelineProps {
   components: React.JSX.Element[];
+  completedMilestones: number[];
 }
 
 function Timeline(props: TimelineProps) {
   return (
     <View>
-      {props.components.map((component: React.JSX.Element) => (
-        <View style={{ flexDirection: 'row' }}>
-          <View style={{ flexDirection: 'column', flex: 2, backgroundColor: 'white', alignItems: 'center' }}>
-            {/* Actual "Line" which makes up the Timeline */}
-            <View style={{ zIndex: 2000, flexGrow: 1, width: 2, backgroundColor: 'grey' }}></View>
-            {/* Container for Timeline "Dot" */}
-            <View style={{ zIndex: 1000, position: 'absolute', width: '100%', height: '100%', backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}>
-              {/* Actual "Dot" on the Timeline */}
-              <View style={{ height: 10, width: 10, backgroundColor: 'grey', borderRadius: 50 }}></View>
+      {props.components.map((component: React.JSX.Element, index: number) => {
+        const completed = props.completedMilestones.includes(index);
+        const timelineColor = completed ? 'blue' : 'grey';
+
+        return (
+          <View style={{ flexDirection: 'row' }}>
+            <View style={{ flexDirection: 'column', flex: 2, backgroundColor: 'white', alignItems: 'center' }}>
+              {/* Actual "Line" which makes up the Timeline */}
+              <View style={{ zIndex: 2000, flexGrow: 1, width: 2, backgroundColor: timelineColor }}></View>
+              {/* Container for Timeline "Dot" */}
+              <View style={{ zIndex: 1000, position: 'absolute', width: '100%', height: '100%', backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}>
+                {/* Actual "Dot" on the Timeline */}
+                <View style={{ height: 10, width: 10, backgroundColor: timelineColor, borderRadius: 50 }}></View>
+              </View>
             </View>
+            {component}
           </View>
-          {component}
-        </View>
-      ))}
+        )
+      })}
     </View>
   );
 }
@@ -37,9 +43,14 @@ function App(): React.JSX.Element {
     <View style={{ flex: 8, backgroundColor: 'yellow', height: 200 }}></View>,
   ];
 
+  const completedMilestones = [0, 1, 2];
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <Timeline components={components} />
+      <Timeline 
+        components={components} 
+        completedMilestones={completedMilestones}
+      />
     </SafeAreaView>
   );
 }

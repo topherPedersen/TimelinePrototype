c8c7c6ae94561fc155475681a2cde9ab712edda2
Chris Pedersen <chrispedatx@gmail.com>
2024-04-06 21:59:45 -0500

    add additional rows

--- App.tsx
+++ App.tsx
@@ -5,11 +5,23 @@ import {
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: 'pink' }}>
+    <SafeAreaView style={{ flex: 1 }}>
       <View>
         <View style={{ flexDirection: 'row' }}>
-          <View style={{ flex: 2, backgroundColor: 'red' }}></View>
+          <View style={{ flex: 2, backgroundColor: 'white' }}></View>
           <View style={{ flex: 8, backgroundColor: 'blue', height: 100 }}></View>
+        </View>
+        <View style={{ flexDirection: 'row' }}>
+          <View style={{ flex: 2, backgroundColor: 'white' }}></View>
+          <View style={{ flex: 8, backgroundColor: 'red', height: 200 }}></View>
+        </View>
+        <View style={{ flexDirection: 'row' }}>
+          <View style={{ flex: 2, backgroundColor: 'white' }}></View>
+          <View style={{ flex: 8, backgroundColor: 'blue', height: 100 }}></View>
+        </View>
+        <View style={{ flexDirection: 'row' }}>
+          <View style={{ flex: 2, backgroundColor: 'white' }}></View>
+          <View style={{ flex: 8, backgroundColor: 'red', height: 200 }}></View>
         </View>
       </View>
     </SafeAreaView>

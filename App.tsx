99a17cce1d4e7220346832c970b4d898c7bf752e
Chris Pedersen <chrispedatx@gmail.com>
2024-04-07 18:36:14 -0500

    make new Timeline component functional

--- App.tsx
+++ App.tsx
@@ -8,68 +8,33 @@ interface TimelineProps {
 }
 
 function Timeline(props: TimelineProps) {
-  // return (
-  //   <View>
-  //     <View style={{ flexDirection: 'row' }}>
-  //       <View style={{ flexDirection: 'column', flex: 2, backgroundColor: 'white', alignItems: 'center' }}>
-  //         {/* Actual "Line" which makes up the Timeline */}
-  //         <View style={{ zIndex: 2000, flexGrow: 1, width: 2, backgroundColor: 'grey' }}></View>
-  //         {/* Container for Timeline "Dot" */}
-  //         <View style={{ zIndex: 1000, position: 'absolute', width: '100%', height: '100%', backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}>
-  //           {/* Actual "Dot" on the Timeline */}
-  //           <View style={{ height: 10, width: 10, backgroundColor: 'grey', borderRadius: 50 }}></View>
-  //         </View>
-  //       </View>
-  //       <View style={{ flex: 8, backgroundColor: 'blue', height: 100 }}></View>
-  //     </View>
-  //     <View style={{ flexDirection: 'row' }}>
-  //       <View style={{ flexDirection: 'column', flex: 2, backgroundColor: 'white', alignItems: 'center' }}>
-  //         {/* Actual "Line" which makes up the Timeline */}
-  //         <View style={{ zIndex: 2000, flexGrow: 1, width: 2, backgroundColor: 'grey' }}></View>
-  //         {/* Container for Timeline "Dot" */}
-  //         <View style={{ zIndex: 1000, position: 'absolute', width: '100%', height: '100%', backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}>
-  //           {/* Actual "Dot" on the Timeline */}
-  //           <View style={{ height: 10, width: 10, backgroundColor: 'grey', borderRadius: 50 }}></View>
-  //         </View>
-  //       </View>
-  //       <View style={{ flex: 8, backgroundColor: 'red', height: 200 }}></View>
-  //     </View>
-  //     <View style={{ flexDirection: 'row' }}>
-  //       <View style={{ flexDirection: 'column', flex: 2, backgroundColor: 'white', alignItems: 'center' }}>
-  //         {/* Actual "Line" which makes up the Timeline */}
-  //         <View style={{ zIndex: 2000, flexGrow: 1, width: 2, backgroundColor: 'grey' }}></View>
-  //         {/* Container for Timeline "Dot" */}
-  //         <View style={{ zIndex: 1000, position: 'absolute', width: '100%', height: '100%', backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}>
-  //           {/* Actual "Dot" on the Timeline */}
-  //           <View style={{ height: 10, width: 10, backgroundColor: 'grey', borderRadius: 50 }}></View>
-  //         </View>
-  //       </View>
-  //       <View style={{ flex: 8, backgroundColor: 'blue', height: 100 }}></View>
-  //     </View>
-  //     <View style={{ flexDirection: 'row' }}>
-  //       <View style={{ flexDirection: 'column', flex: 2, backgroundColor: 'white', alignItems: 'center' }}>
-  //         {/* Actual "Line" which makes up the Timeline */}
-  //         <View style={{ zIndex: 2000, flexGrow: 1, width: 2, backgroundColor: 'grey' }}></View>
-  //         {/* Container for Timeline "Dot" */}
-  //         <View style={{ zIndex: 1000, position: 'absolute', width: '100%', height: '100%', backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}>
-  //           {/* Actual "Dot" on the Timeline */}
-  //           <View style={{ height: 10, width: 10, backgroundColor: 'grey', borderRadius: 50 }}></View>
-  //         </View>
-  //       </View>
-  //       <View style={{ flex: 8, backgroundColor: 'red', height: 200 }}></View>
-  //     </View>
-  //   </View>
-  // );
-  return props.components.map((component: React.JSX.Element) => component);
+  return (
+    <View>
+      {props.components.map((component: React.JSX.Element) => (
+        <View style={{ flexDirection: 'row' }}>
+          <View style={{ flexDirection: 'column', flex: 2, backgroundColor: 'white', alignItems: 'center' }}>
+            {/* Actual "Line" which makes up the Timeline */}
+            <View style={{ zIndex: 2000, flexGrow: 1, width: 2, backgroundColor: 'grey' }}></View>
+            {/* Container for Timeline "Dot" */}
+            <View style={{ zIndex: 1000, position: 'absolute', width: '100%', height: '100%', backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}>
+              {/* Actual "Dot" on the Timeline */}
+              <View style={{ height: 10, width: 10, backgroundColor: 'grey', borderRadius: 50 }}></View>
+            </View>
+          </View>
+          {component}
+        </View>
+      ))}
+    </View>
+  );
 }
 
 function App(): React.JSX.Element {
 
   const components = [
-    <View style={{ flex: 8, backgroundColor: 'blue', height: 100 }}></View>,
-    <View style={{ flex: 8, backgroundColor: 'red', height: 200 }}></View>,
-    <View style={{ flex: 8, backgroundColor: 'blue', height: 100 }}></View>,
-    <View style={{ flex: 8, backgroundColor: 'red', height: 200 }}></View>,
+    <View style={{ flex: 8, backgroundColor: 'purple', height: 100 }}></View>,
+    <View style={{ flex: 8, backgroundColor: 'yellow', height: 200 }}></View>,
+    <View style={{ flex: 8, backgroundColor: 'purple', height: 100 }}></View>,
+    <View style={{ flex: 8, backgroundColor: 'yellow', height: 200 }}></View>,
   ];
 
   return (

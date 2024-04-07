2d06aa46c01007e9ef8636b26fe63a4fb303accf
Chris Pedersen <chrispedatx@gmail.com>
2024-04-07 15:53:03 -0500

    remove todo comment

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import {
   SafeAreaView, View,
 } from 'react-native';
 
-// TODO: Try and add a prototype of a timeline component to the first column below
 function App(): React.JSX.Element {
   return (
     <SafeAreaView style={{ flex: 1 }}>

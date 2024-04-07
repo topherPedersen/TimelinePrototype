43e04af1e56aa2123f5d3d41f9a7cb944f804742
Chris Pedersen <chrispedatx@gmail.com>
2024-04-07 15:45:47 -0500

    finish rending timeline, is now displaying a line with dots all in the right place

--- App.tsx
+++ App.tsx
@@ -21,15 +21,39 @@ function App(): React.JSX.Element {
           <View style={{ flex: 8, backgroundColor: 'blue', height: 100 }}></View>
         </View>
         <View style={{ flexDirection: 'row' }}>
-          <View style={{ flex: 2, backgroundColor: 'white' }}></View>
+          <View style={{ flexDirection: 'column', flex: 2, backgroundColor: 'white', alignItems: 'center' }}>
+            {/* Actual "Line" which makes up the Timeline */}
+            <View style={{ zIndex: 2000, flexGrow: 1, width: 2, backgroundColor: 'grey' }}></View>
+            {/* Container for Timeline "Dot" */}
+            <View style={{ zIndex: 1000, position: 'absolute', width: '100%', height: '100%', backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}>
+              {/* Actual "Dot" on the Timeline */}
+              <View style={{ height: 10, width: 10, backgroundColor: 'grey', borderRadius: 50 }}></View>
+            </View>
+          </View>
           <View style={{ flex: 8, backgroundColor: 'red', height: 200 }}></View>
         </View>
         <View style={{ flexDirection: 'row' }}>
-          <View style={{ flex: 2, backgroundColor: 'white' }}></View>
+          <View style={{ flexDirection: 'column', flex: 2, backgroundColor: 'white', alignItems: 'center' }}>
+            {/* Actual "Line" which makes up the Timeline */}
+            <View style={{ zIndex: 2000, flexGrow: 1, width: 2, backgroundColor: 'grey' }}></View>
+            {/* Container for Timeline "Dot" */}
+            <View style={{ zIndex: 1000, position: 'absolute', width: '100%', height: '100%', backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}>
+              {/* Actual "Dot" on the Timeline */}
+              <View style={{ height: 10, width: 10, backgroundColor: 'grey', borderRadius: 50 }}></View>
+            </View>
+          </View>
           <View style={{ flex: 8, backgroundColor: 'blue', height: 100 }}></View>
         </View>
         <View style={{ flexDirection: 'row' }}>
-          <View style={{ flex: 2, backgroundColor: 'white' }}></View>
+          <View style={{ flexDirection: 'column', flex: 2, backgroundColor: 'white', alignItems: 'center' }}>
+            {/* Actual "Line" which makes up the Timeline */}
+            <View style={{ zIndex: 2000, flexGrow: 1, width: 2, backgroundColor: 'grey' }}></View>
+            {/* Container for Timeline "Dot" */}
+            <View style={{ zIndex: 1000, position: 'absolute', width: '100%', height: '100%', backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}>
+              {/* Actual "Dot" on the Timeline */}
+              <View style={{ height: 10, width: 10, backgroundColor: 'grey', borderRadius: 50 }}></View>
+            </View>
+          </View>
           <View style={{ flex: 8, backgroundColor: 'red', height: 200 }}></View>
         </View>
       </View>
